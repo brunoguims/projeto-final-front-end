4598c6271e0ca0324a0dbe5a35b77c2a5b5e2d08
marcellmoura <marcelltmoura@gmail.com>
2024-11-25 16:49:52 -0300

    criacao do carrossel da culinaria italiana e do html das receitas

--- scripts.js
+++ scripts.js
@@ -12,3 +12,18 @@ carouselBrasil.addEventListener('mouseenter', function () {
 carouselBrasil.addEventListener('mouseleave', function () {
     carousel.pause();
 });
+
+var carouselItalia = document.getElementById('carouselItalia');
+var carousel = new bootstrap.Carousel(carouselItalia, {
+    interval: 2000,
+    pause: false,
+    ride: false
+});
+
+carouselItalia.addEventListener('mouseenter', function () {
+    carousel.cycle();
+});
+
+carouselItalia.addEventListener('mouseleave', function () {
+    carousel.pause();
+});
